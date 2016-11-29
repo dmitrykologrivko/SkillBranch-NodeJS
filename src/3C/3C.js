4ddc8f0dff4d75c648e1a01bad183b06dbf95cd3
Dmitry Kologrivko <dmitrykologrivko@gmail.com>
2016-11-29 14:09:43 +0300

    refactoring for task 3C

--- src/3C/3C.js
+++ src/3C/3C.js
@@ -6,20 +6,53 @@ const PORT = 3000;
 // Load pokemons from JSON file
 const pokemons = require('./pokemons.json');
 
-function compareByKey(key) {
-    return function compare(a, b) {
-        return a[key] < b[key] ? -1 : a[key] > b[key] ? 1 : a.name < b.name ? -1 : a.name > b.name ? 1 : 0;
+// fat - max(pokemon.weight / pokemon.height)
+// angular - min(pokemon.weight / pokemon.height)
+// heavy - max(pokemon.weight)
+// light - min(pokemon.weight)
+// huge - max(pokemon.height)
+// micro - min(pokemon.height)
+// MAX parameter will be a negative number, and tend to zero when we will be sort
+// MIN parameter will be without changes, and tend to zero when we will be sort
+pokemons.forEach(p => {
+    p.fat = -p.weight / +p.height;
+    p.angular = +p.weight / +p.height;
+    p.heavy = -p.weight;
+    p.light = +p.weight;
+    p.huge = -p.height;
+    p.micro = +p.height;
+});
+
+function sortByTag(tag) {
+    function nameComparator(a, b) {
+        return a.name < b.name ? -1 : a.name > b.name ? 1 : 0
+    }
+
+    function tagComparator(a, b) {
+        return a[tag] < b[tag] ? -1 : a[tag] > b[tag] ? 1 : a.name < b.name ? -1 : a.name > b.name ? 1 : 0;
+    }
+
+    if (!tag) return;
+
+    if (tag === 'name') {
+        pokemons.sort(nameComparator);
+    } else {
+        pokemons.sort(tagComparator);
     }
 }
 
-function compareByName(a, b) {
-    return a < b ? -1 : a > b ? 1 : 0;
+function getOffset(limit, offset) {
+    return pokemons.slice(offset, offset + limit).map(pokemon => {
+        return pokemon.name;
+    });
 }
 
 const app = express();
 
 // Config express
 app.use(cors());
+
+// Setup middlewares
 app.use((req, res, next) => {
     req.limit = req.query.limit || 20;
     req.offset = req.query.offset || 0;
@@ -27,24 +60,28 @@ app.use((req, res, next) => {
 });
 
 // Setup routes
-app.use((req, res, next) => {
-    pokemons.forEach(p => {
-        p.fat = -p.weight / +p.height;
-        p.angular = +p.weight / +p.height;
-        p.heavy = -p.weight;
-        p.light = +p.weight;
-        p.huge = -p.height;
-        p.micro = +p.height;
-    });
-    next();
+app.get('/', (req, res) => {
+    sortByTag('name');
+    res.json(getOffset(+req.limit, +req.offset));
 });
 
-app.get(/^\/[^\/]*$/, (req, res) => {
-    const Url = req.url.match(/^\/([^\/\?]*)(?:.*)/)[1];
-    const ans = pokemons.sort(compareByKey(Url)).slice(req.offset, +req.offset + +req.limit);
-    res.send(ans.map(value => {
-        return value.name;
-    }));
+app.get('/:type', (req, res) => {
+    const TAGS = [
+        'fat',
+        'angular',
+        'heavy',
+        'light',
+        'huge',
+        'micro'
+    ];
+
+    if (req.params && req.params.type && TAGS.includes(req.params.type)) {
+        sortByTag(req.params.type);
+    } else {
+        sortByTag('name');
+    }
+
+    res.json(getOffset(+req.limit, +req.offset));
 });
 
 // 404 catch-all handler (middleware)
